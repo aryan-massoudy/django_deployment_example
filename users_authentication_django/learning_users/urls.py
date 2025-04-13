fc0d9f291d63f9f85e4aa8b956f1377733ad03b4
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-04-13 21:57:47 +0200

    fixed profile pic url accessing

--- users_authentication_django/learning_users/urls.py
+++ users_authentication_django/learning_users/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url,include
+""" from django.conf.urls import url,include
 from django.contrib import admin
 from basic_app import views
 
@@ -24,3 +24,20 @@ urlpatterns = [
     url(r'^basic_app/',include('basic_app.urls')),
     url(r'^logout/$', views.user_logout, name='logout'),
 ]
+ """
+from django.conf.urls import url, include
+from django.contrib import admin
+from basic_app import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^special/', views.special, name='special'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^basic_app/', include('basic_app.urls')),
+    url(r'^logout/$', views.user_logout, name='logout'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
